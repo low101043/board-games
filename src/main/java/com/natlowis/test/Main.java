cadb737ae441caf48df81616ba4e6055028a940c
Nathaniel Lowis <nathaniel@lowis.co.uk>
2021-01-20 11:54:58 +0000

    Updated test file

--- src/main/java/com/natlowis/test/Main.java
+++ src/main/java/com/natlowis/test/Main.java
@@ -1,5 +1,8 @@
 package com.natlowis.test;
 
+import com.natlowis.connectfour.BoardConnectFour;
+import com.natlowis.connectfour.PlayConnectFour;
+import com.natlowis.interfaces.Play;
 import com.natlowis.naughtsandcrosses.PlayNaughtsAndCrosses;
 
 public class Main {
@@ -10,7 +13,9 @@ public class Main {
 		PlayNaughtsAndCrosses play = new PlayNaughtsAndCrosses(); 
 		System.out.println("HEllo");
 		
-		play.run();
+		Play four = new PlayConnectFour();
+		
+		four.run();
 		
 		System.out.println("Finished");
 
