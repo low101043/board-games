d13c76eaa7ea15b9210e4b39f0062a393ae54d80
Nathaniel Lowis <nathaniel@lowis.co.uk>
2021-05-21 15:50:12 +0100

    Added chess board

---
+++ src/main/java/com/natlowis/games/game/chess/BoardChess.java
@@ -0,0 +1,33 @@
+package com.natlowis.games.game.chess;
+
+import com.natlowis.games.game.Type;
+import com.natlowis.games.game.interfaces.Board;
+import com.natlowis.games.game.interfaces.Piece;
+
+public class BoardChess implements Board {
+
+	@Override
+	public Type won() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean add(Piece input, int i, int j) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void remove(Piece input, int i, int j) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Piece[][] currentBoard() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
